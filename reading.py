0f9ca0bcff62dcbb786a286ff906da2bcf9e2bc5
lemikhovalex <42550446+lemikhovalex@users.noreply.github.com>
2019-04-15 00:40:08 +0300

    Create reading.py

---
+++ reading.py
@@ -0,0 +1,16 @@
+def reading_dataset(file_name, n, d):
+    out = [0] * n
+    for i in range(n):
+        out[i] = [0] * (d+1)
+    f = open(file_name, 'r')
+    for i in range(n):
+        s=f.readline()
+        tokens=s.split(' ')
+        out[i][0]=tokens[0]
+        del tokens[0]
+        while len(tokens)>1:
+            print(tokens)
+            num_and_val=tokens[0].split(':')
+            out[i][ int(num_and_val[0]) ]= num_and_val[1]
+            del tokens[0]
+    return out
